9c49db8c75770dc177ec09ddd503d6e74893f48b
Lucas Feitosa <universenerd249@gmail.com>
2022-05-07 15:57:52 -0300

    adicionar arquivos

---
+++ main.py
@@ -0,0 +1,75 @@
+import pygame
+from pygame.locals import *
+import math
+
+
+pygame.init()
+
+WIDTH = HEIGHT = 640
+WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('UCM')
+
+FPS = 30
+timer = pygame.time.Clock()
+
+running = True
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (240, 20, 20)
+BLUE = (20, 20, 240)
+RADIUS = 180
+CENTER = (640 / 2, 640 / 2)
+
+
+class Ball:
+    def __init__(self):
+        self.color = RED
+        self.initial_pos = (640 / 2, 640 / 2 - RADIUS)
+        self.pos_x = self.initial_pos[0]
+        self.pos_y = self.initial_pos[1]
+        self.radius = 20
+        self.particle = pygame.draw.circle(WINDOW, self.color, self.initial_pos, self.radius)
+        self.angular_speed = 0.05
+        self.angle = 0
+        self.velocity = 0
+        self.main_radius = RADIUS
+        self.velocity_x = 0
+        self.velocity_y = 0
+
+    def tick(self):
+        self.velocity = self.angular_speed * self.main_radius
+        self.velocity_x = math.cos(self.angle) * self.velocity
+        self.velocity_y = math.sin(self.angle) * self.velocity
+        self.pos_x += self.velocity_x
+        self.pos_y += self.velocity_y
+        print(self.distance_to_center())
+        self.particle = pygame.draw.circle(WINDOW, self.color, (self.pos_x, self.pos_y), self.radius)
+        self.angle += self.angular_speed
+
+    def distance_to_center(self):
+        main_center = CENTER
+        dx = main_center[0] - self.pos_x
+        dy = main_center[1] - self.pos_y
+        x = math.sqrt(dx ** 2 + dy ** 2)
+        return x
+
+
+ball = Ball()
+
+while running:
+    WINDOW.fill(BLACK)
+    timer.tick(FPS)
+
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            running = False
+
+    if not running:
+        break
+
+    main_circle = pygame.draw.circle(WINDOW, RED, CENTER, radius=RADIUS, width=1)
+    ball.tick()
+
+    pygame.display.flip()
